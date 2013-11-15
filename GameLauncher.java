fb0e15503d65a5c19d5ada3bd796ab90c62703b5
NLSteveO <steveokeefe88@gmail.com>
2013-11-15 00:47:49 -0330

    More of the menu options are activated.

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,17 +1,20 @@
+import javax.swing.BoxLayout;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.SwingUtilities;
+
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class GameLauncher extends JFrame{
 
@@ -52,11 +55,33 @@ public class GameLauncher extends JFrame{
     
     public JMenuItem createSaveItem(){
     	save = new JMenuItem("Save");
+    	class MenuItemListener implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+   				    JFileChooser fc = new JFileChooser("./Saves/");
+                int returnVal = fc.showSaveDialog(GameLauncher.this);
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+   						      File file = fc.getSelectedFile();
+                    }
+            }
+         }
+         ActionListener listener = new MenuItemListener();
+         save.addActionListener(listener);
     	return save;
     }
     
     public JMenuItem createLoadItem(){
     	load = new JMenuItem("Load");
+        class MenuItemListener implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+                JFileChooser fc = new JFileChooser("./Saves/");
+   					 int returnVal = fc.showOpenDialog(GameLauncher.this);
+   					     if (returnVal == JFileChooser.APPROVE_OPTION){
+   						      File file = fc.getSelectedFile();
+   						  }
+            }
+         }
+         ActionListener listener = new MenuItemListener();
+         load.addActionListener(listener);
     	return load;
     }
     
@@ -92,11 +117,28 @@ public class GameLauncher extends JFrame{
     
     public JMenuItem createNewChar(){
     	nChar = new JMenuItem("New Character");
+        class MenuItemListener implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+            }
+         }
+         ActionListener listener = new MenuItemListener();
+         nChar.addActionListener(listener);
     	return nChar;
     }
     
     public JMenuItem createEditChar(){
     	eChar = new JMenuItem("Edit Character");
+        class MenuItemListener implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+   					JFileChooser fc = new JFileChooser("./Characters/");
+   					int returnVal = fc.showOpenDialog(GameLauncher.this);
+   					if (returnVal == JFileChooser.APPROVE_OPTION){
+   					    File file = fc.getSelectedFile();
+   					}
+            }
+         }
+         ActionListener listener = new MenuItemListener();
+         eChar.addActionListener(listener);
     	return eChar;
     }
     
@@ -109,11 +151,48 @@ public class GameLauncher extends JFrame{
     
     public JMenuItem createRulesFrame(){
     	rules = new JMenuItem("Rules");
+        class MenuItemListener implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+               JFrame rulesFrame = new JFrame();
+               JPanel rulesPanel = new JPanel();
+               rulesPanel.add(new JLabel("The"));
+               rulesPanel.add(new JLabel("Rules"));
+               rulesPanel.add(new JLabel("Will"));
+               rulesPanel.add(new JLabel("Go"));
+               rulesPanel.add(new JLabel("Here!"));
+               rulesFrame.add(rulesPanel);
+               rulesFrame.setSize(250, 150);
+               rulesFrame.setTitle("Rules");
+               rulesFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+               rulesFrame.setVisible(true);
+            }
+         }
+         ActionListener listener = new MenuItemListener();
+         rules.addActionListener(listener);
     	return rules;
     }
     
     public JMenuItem createAboutFrame(){
     	about = new JMenuItem("About");
+        class MenuItemListener implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+               JFrame aboutFrame = new JFrame();
+               JPanel main = new JPanel();
+               main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+               main.add(new JLabel("About:"));
+               main.add(new JLabel("Created by: Stephen O'Keefe, Brandon Bemister, Tim Murray, and Scott Maher"));
+               main.add(new JLabel("Student Number: 200644979, 201121159, 201129574, and 201004603"));
+               main.add(new JLabel("Created for Computer Science 3716"));
+               main.add(new JLabel("Instructor: Dr. Fiech"));
+               aboutFrame.add(main);
+               aboutFrame.setSize(550, 150);
+               aboutFrame.setTitle("About");
+               aboutFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+               aboutFrame.setVisible(true);
+            }
+         }
+         ActionListener listener = new MenuItemListener();
+         about.addActionListener(listener);
     	return about;
     }
         

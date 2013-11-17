9eb18f2457f4457eb2f3e00bbeb85ee6abbf4f5d
bjbemister <bjb541@mun.ca>
2013-11-17 00:40:11 -0330

    Some very minor updates

--- Character.java
+++ Character.java
@@ -3,18 +3,26 @@ package game.character;
 public class Character{
 
     private Happiness happy;
-    private Teritory location;
+    //private Teritory location;
+    //private Status stat;
+    private String name;
     
-    public Character{
-        
+    public Character(String aName, Happiness h){
+        //stat = new Status("Civ");
+        name = aName;
+        happy = h;
     }
 
-    public void move(Teritory t){
+    //public void move(Teritory t){
       
-    }
+    //}
 
     public Happiness getHappiness(){
-        
+        return happy;
+    }
+    
+    public String getName(){
+    	return name;
     }
 
     public void startCountry(){

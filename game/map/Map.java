f93fa5a19da92d31f38ff67090d35c64bc765e39
ScottMaher <slm586@mun.ca>
2013-11-17 20:06:42 -0330

    Sorry about all the crazy pushes, had some errors to work out

---
+++ game/map/Map.java
@@ -0,0 +1,98 @@
+package game.map;
+
+import java.util.Random;
+import java.util.ArrayList;
+public class Map{
+	
+	private Coord[][] coord;
+    private ArrayList<Territory> territories;
+    private ArrayList<Character> characters;
+    
+    //For now, generate one of several preset maps, in future iterations random
+    //map generation may be implemented.
+    public Map(int numTer, int size){
+    	for(int i=0;i<size;i++){
+    		for(int j=0;j<size;j++){
+    			coord[i][j]= new Coord(i,j);
+    		}
+    	} 
+    	createMap(numTer);
+    	fillNeighbours();
+    	
+    }
+    
+    // create a map according to an algorithm
+    // for now it will be a standard map, in future iterations 
+    // a more varied and random map will be drawn
+    
+    
+    public void  createMap(int numTer){
+    	if(numTer>coord.length)
+    		return; // Error Check Better!!!
+    	int x,y;
+    	Random r= new Random();
+    		for(int i=0; i<numTer;i++){
+    			x=r.nextInt(coord.length);
+    			y=r.nextInt(coord.length);
+    			System.out.println("("+x+","+y+")");
+    			coord[x][y].setTerritory(new Territory());
+    			System.out.println("Territory:"+coord[x][y].getTerritory().getID());
+    		}
+    }
+    
+    public void fillNeighbours(){
+    	for(int i =0; i< coord.length;i++){
+    		for(int j= 0; j< coord.length;j++){
+    			if(coord[i][j].hasTerritory()){
+    				
+    				if(coord[i-1][j-1].hasTerritory())
+    					dualNeighbourship(i,j,i-1,j-1);
+    				if(coord[i][j-1].hasTerritory())
+    					dualNeighbourship(i,j,i,j-1);
+    				if(coord[i+1][j-1].hasTerritory())
+    					dualNeighbourship(i,j,i+1,j-1);
+    				if(coord[i-1][j].hasTerritory())
+    					dualNeighbourship(i,j,i-1,j);
+    				if(coord[i+1][j].hasTerritory())
+    					dualNeighbourship(i,j,i+1,j);
+    				if(coord[i-1][j+1].hasTerritory())
+    					dualNeighbourship(i,j,i-1,j+1);
+    				if(coord[i][j+1].hasTerritory())
+    					dualNeighbourship(i,j,i,j+1);
+    				if(coord[i+1][j+1].hasTerritory())
+    					dualNeighbourship(i,j,i+1,j+1);
+    				
+    			}
+    				
+    		}
+    	}
+    }
+    
+    public void dualNeighbourship(int i, int j, int k, int l){
+				coord[i][j].getTerritory().addNeighbour(coord[k][l].getTerritory());
+				coord[k][l].getTerritory().addNeighbour(coord[i][j].getTerritory());
+	}
+    
+
+    public void drawMap(){
+        
+    }
+
+    public void updateMap(){
+        
+    }
+
+    public ArrayList<Territory> legalMoves(Territory t){
+        ArrayList<Territory> ans = t.getNeighbours();
+        return ans;
+    }
+    
+    public boolean isLegalMove(Territory start, Territory end){
+    	ArrayList<Territory> check = start.getNeighbours();
+    	for(int i=0; i<check.size();i++){
+    		if(check.get(i).equals(end))
+    			return true;
+    	}
+    	return false;
+    }
+}
---
+++ game/map/Territory.java
@@ -0,0 +1,65 @@
+package game.map;
+import java.util.ArrayList;
+
+public class Territory {
+	
+
+	private int ID;
+    private ArrayList<Territory> neighbours;
+    private IdGen generator = IdGen.getIdGen(); 
+    
+    public Territory(){
+    	ID=generator.getID();
+    	neighbours = new ArrayList<Territory>();
+    }
+    
+    public Territory(Territory t){
+    	ID=generator.getID();
+    	neighbours= new ArrayList<Territory>();
+    	neighbours.add(t);
+    }
+    
+    public ArrayList<Territory> getNeighbours(){
+        return neighbours;
+    }
+    
+    public int numNeighbours(){
+    	return neighbours.size();
+    }
+    
+    public boolean isNeighbour(Territory query){
+        for(int i=0;i<neighbours.size();i++){
+            if(neighbours.get(i).equals(query))
+            	return true;
+        }
+        return false;
+    }
+    
+
+    public int getID(){
+    	return ID;
+
+
+
+    public boolean addNeighbour(Territory t){
+    	for(int i=0; i<neighbours.size();i++){
+    		if(neighbours.get(i)==null)
+    			neighbours.set(i, t);
+    			return true;
+    	}
+    	 return false;
+
+    }
+    
+    public boolean equals(Territory t){
+    	if(t.getID()==this.getID())
+    		return true;
+    	return false;
+    }
+    
+    public void addNeighbour(Territory t){
+    			neighbours.add(t);
+  
+    }
+    
+   }

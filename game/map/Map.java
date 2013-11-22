f8b27c8bb66f782c0522e5e9699050ac94d5fe3e
ScottMaher <slm586@mun.ca>
2013-11-22 15:20:56 -0330

    Re done the map with the associations that he wanted, its not drawn yet

--- game/map/Map.java
+++ game/map/Map.java
@@ -2,42 +2,22 @@ package game.map;
 
 import game.character.Character;
 
-import java.util.Random;
 import java.util.ArrayList;
 public class Map{
 	
-	private Coord[][] coord;
-	private int size;
+	
     private ArrayList<Character> characters;
+    private ArrayList<Territory> terr;
     
     
-    public Map(int numTer, int size){
-    	this.size = size;
-    	coord = new Coord[size][size];
-    	for(int i=0;i<size;i++){
-    		for(int j=0;j<size;j++){
-    			coord[i][j] = new Coord(i, j);
-    		}
-    	} 
-    	createMap(numTer);
-    	fillNeighbours();
-    	
+    public Map(){
+    	createMap();
     }
     
     // place a character randomly by picking random squares until a territory square is found 
     // and setting a character there
     public void placeCharacter(Character c){
-    	boolean truth = true;
-    	Random r = new Random();
-    	while(truth){
-    		int x = r.nextInt(size);
-    		int y = r.nextInt(size);
-    		if(coord[x][y].hasTerritory()){
-    			c.setLocation(coord[x][y].getTerritory());
-    			characters.add(c);
-    			truth=false;
-    		}
-    	}
+    
     }
     
     // place a character on the specified territory
@@ -45,65 +25,103 @@ public class Map{
     	c.setLocation(t);
     	characters.add(c);
     }
-    
-    public Map(Coord[][] c){
-    	coord = c;
-    	fillNeighbours();
-    }
-    
+ 
     // create a map according to an algorithm
     // for now it will be a standard map, in future iterations 
     // a more varied and random map will be drawn
     
-    public void  createMap(int numTer){
-    	if(numTer>coord.length*coord.length)
-    		return; // Error Check Better!!!
-    	int x,y;
-    	Random r= new Random();
-    		for(int i=0; i<numTer;i++){
-    			x=r.nextInt(coord.length);
-    			y=r.nextInt(coord.length);
-    			//System.out.print("("+x+","+y+"): ");
-    			coord[x][y].setTerritory(new Territory(coord[x][y]));
-    			//System.out.println("Territory:"+coord[x][y].getTerritory().getID());
-    		}
+    public void neighbours(Territory t, Territory y){
+    	t.addNeighbour(y);
+    	y.addNeighbour(t);
     }
     
-    public void fillNeighbours(){
-    	for(int i =0; i< coord.length;i++){
-    		for(int j= 0; j< coord.length;j++){
-    			if(coord[i][j].hasTerritory()){
-    				try{
-    					if(coord[i-1][j-1].hasTerritory())
-    						dualNeighbourship(i,j,i-1,j-1);
-    					if(coord[i][j-1].hasTerritory())
-    						dualNeighbourship(i,j,i,j-1);
-    					if(coord[i+1][j-1].hasTerritory())
-    						dualNeighbourship(i,j,i+1,j-1);
-    					if(coord[i-1][j].hasTerritory())
-    						dualNeighbourship(i,j,i-1,j);
-    					if(coord[i+1][j].hasTerritory())
-    						dualNeighbourship(i,j,i+1,j);
-    					if(coord[i-1][j+1].hasTerritory())
-    						dualNeighbourship(i,j,i-1,j+1);
-    					if(coord[i][j+1].hasTerritory())
-    						dualNeighbourship(i,j,i,j+1);
-    					if(coord[i+1][j+1].hasTerritory())
-    						dualNeighbourship(i,j,i+1,j+1);
-    				}
-    				catch(ArrayIndexOutOfBoundsException e){
-    					continue;
-    				}
-    			}
-    				
-    		}
-    	}
-    }
+    // create the predetermined complicated map thing.
+    private void  createMap(){
+    	terr.add(new Territory("Northrend"));
+    	terr.add(new Territory("Windhelm"));
+    	neighbours(terr.get(0),terr.get(1));
+    	terr.add(new Territory("Westros"));
+    	neighbours(terr.get(0),terr.get(2));
+    	neighbours(terr.get(1),terr.get(2));
+    	terr.add(new Territory("Midway"));
+    	neighbours(terr.get(1),terr.get(3));
+    	neighbours(terr.get(2),terr.get(3));
+    	terr.add(new Territory("Rampart"));
+    	neighbours(terr.get(4),terr.get(2));
+    	terr.add(new Territory("Verth"));
+    	neighbours(terr.get(4),terr.get(5));
+    	neighbours(terr.get(3),terr.get(5));
+    	neighbours(terr.get(2),terr.get(5));
+    	terr.add(new Territory("Holdout"));
+    	neighbours(terr.get(1),terr.get(6));
+    	neighbours(terr.get(3),terr.get(6));
+    	neighbours(terr.get(5),terr.get(6));
+    	terr.add(new Territory("Gyldenhul"));
+    	neighbours(terr.get(4),terr.get(7));
+    	neighbours(terr.get(5),terr.get(7));
+    	neighbours(terr.get(6),terr.get(7));
+    	terr.add(new Territory("Twins"));
+    	neighbours(terr.get(7),terr.get(8));
+    	
+    	terr.add(new Territory("Solitude"));
+    	neighbours(terr.get(8),terr.get(9));
+    	
+    	terr.add(new Territory("Power"));
+    	terr.add(new Territory("Wisdom"));
+    	terr.add(new Territory("Courage"));
+    	neighbours(terr.get(9),terr.get(11));
+    	neighbours(terr.get(10),terr.get(11));
+    	neighbours(terr.get(12),terr.get(11));
+    	neighbours(terr.get(10),terr.get(12));
+
+    	terr.add(new Territory("Orange Isle"));
+    	terr.add(new Territory("Red Isle"));
+    	terr.add(new Territory("Green Isle"));
+    	neighbours(terr.get(1),terr.get(13));
+    	neighbours(terr.get(13),terr.get(14));
+    	neighbours(terr.get(13),terr.get(15));
+    	neighbours(terr.get(14),terr.get(15));
+    	neighbours(terr.get(10),terr.get(15));
+
+    	terr.add(new Territory("Mobius"));
+    	neighbours(terr.get(16),terr.get(12));
+    	terr.add(new Territory("Horus"));
+    	neighbours(terr.get(16),terr.get(17));
+    	terr.add(new Territory("Vayne"));
+    	neighbours(terr.get(17),terr.get(18));
+    	terr.add(new Territory("Gallows"));
+    	neighbours(terr.get(18),terr.get(19));
+    	
+    	terr.add(new Territory("SouthShield"));
+    	neighbours(terr.get(19),terr.get(20));
+    	terr.add(new Territory("Rearguard"));
+    	neighbours(terr.get(20),terr.get(21));
+    	terr.add(new Territory("Hearth"));
+    	neighbours(terr.get(21),terr.get(22));
+    	neighbours(terr.get(20),terr.get(22));
+    	terr.add(new Territory("Misery"));
+    	neighbours(terr.get(22),terr.get(23));
+    	neighbours(terr.get(21),terr.get(23));
+    	terr.add(new Territory("Vanguard"));
+    	neighbours(terr.get(23),terr.get(24));
+    	neighbours(terr.get(22),terr.get(24));
+    	neighbours(terr.get(21),terr.get(24));
+    	terr.add(new Territory("Helgan"));
+    	neighbours(terr.get(22),terr.get(25));
+    	terr.add(new Territory("Ococo"));
+    	neighbours(terr.get(25),terr.get(26));
+    	neighbours(terr.get(22),terr.get(26));
+    	terr.add(new Territory("Primm"));
+    	neighbours(terr.get(26),terr.get(27));
+    	terr.add(new Territory("Brill"));
+    	neighbours(terr.get(26),terr.get(28));
+    	neighbours(terr.get(27),terr.get(28));
+    	neighbours(terr.get(14),terr.get(28));
+    	
+    	
+    } 
     
-    public void dualNeighbourship(int i, int j, int k, int l){
-				coord[i][j].getTerritory().addNeighbour(coord[k][l].getTerritory());
-				coord[k][l].getTerritory().addNeighbour(coord[i][j].getTerritory());
-	}
+    
     
     public ArrayList<Territory> legalMoves(Territory t){
         ArrayList<Territory> ans = t.getNeighbours();
@@ -125,13 +143,5 @@ public class Map{
     			return true;
     	}
     	return false;
-    }
-    
-    public int getSize(){
-    	return size;
-    }
-    
-    public Coord[][] getCoordinates(){
-    	return coord;
     }
 }

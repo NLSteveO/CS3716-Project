95302593a4ea76ff7f2b57d8a8e7849aa5fa6df8
ScottMaher <slm586@mun.ca>
2013-11-26 16:29:17 -0330

    Uses doubles now like it should

--- game/map/Area.java
+++ game/map/Area.java
@@ -1,30 +1,30 @@
 package game.map;
-import java.awt.Point;
-
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.util.ArrayList;
 
 public class Area {
 	
-	private ArrayList<Point> corners;
+	private ArrayList<Point2D.Double> corners;
 	int terrSides;
 	
-	public Area(int sides,int[] x, int [] y){
+	public Area(int sides,double[] x, double [] y){
 		if(x.length!=y.length){
 			return;
 		}
 		terrSides=sides;
 		for(int i=0; i<x.length;i++){
-			corners.add(new Point(x[i],y[i]));
+			corners.add(new Point2D.Double(x[i],y[i]));
 		}
 		
 	}
-	public boolean isInside(int x, int y){
+	public boolean isInside(double x, double y){
 		if(terrSides==0)
 			return isInsideCirc(x,y);
 		return isInsideCorn(x,y);
 	}
 	
-	public boolean isInsideCirc(int x, int y){
+	public boolean isInsideCirc(double x, double y){
 		if((x-corners.get(0).getX())*(x-corners.get(0).getX())+(y-corners.get(0).getY())*(y-corners.get(0).getY())<30*30){
 			return true;
 		}
@@ -32,7 +32,7 @@ public class Area {
 		
 	}
 	//implementation of complex polygon algorithm
-	public boolean isInsideCorn(int x,int y){
+	public boolean isInsideCorn(double x,double y){
 		int i, j = terrSides-1;
 		boolean oddNodes = false;
 		
@@ -40,9 +40,9 @@ public class Area {
 			if(corners.get(i).getY()<y && corners.get(j).getY()>=y
 					|| corners.get(j).getY()<y && corners.get(i).getY()>=y){
 				
-						if((corners.get(i).getX()+(y-corners.get(i).getY())
+						if(corners.get(i).getX()+(y-corners.get(i).getY())
 								/ (corners.get(j).getY()-corners.get(i).getY())
-									*(corners.get(j).getX()-corners.get(i).getX()))<x){
+									*(corners.get(j).getX()-corners.get(i).getX())<x){
 									oddNodes=!oddNodes;
 						}
 			}
